db0416a7842867173378bfde6f54db99e9fe7994
luckyfinance-dev <78893395+luckyfinance-dev@users.noreply.github.com>
2021-02-24 15:13:47 -0800

    Update colors.ts

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -2,10 +2,10 @@ import { Colors } from "./types";
 
 export const baseColors = {
   failure: "#ED4B9E",
-  primary: "#aa8929",
-  primaryBright: "#aa8929",
-  primaryDark: "#aa8929",
-  secondary: "#75332e",
+  primary: "#8a6a10",
+  primaryBright: "#67500c",
+  primaryDark: "#8f6e10",
+  secondary: "#a56227",
   success: "#31D0AA",
   warning: "#FFB237",
 };
@@ -17,32 +17,32 @@ export const brandColors = {
 export const lightColors: Colors = {
   ...baseColors,
   ...brandColors,
-  background: "#e4f0f4",
+  background: "#FAF9FA",
   backgroundDisabled: "#E9EAEB",
-  contrast: "#eef6f8",
+  contrast: "#191326",
   invertedContrast: "#FFFFFF",
   input: "#eeeaf4",
-  tertiary: "#e6dbbe",
-  text: "#351a17",
-  textDisabled: "#BDC2C4",
-  textSubtle: "#8f80ba",
-  borderColor: "#e1edf3",
+  tertiary: "#EFF4F5",
+  text: '#69552e',
+  textDisabled: '#998c83',
+  textSubtle: '#836a38',
+  borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
-    bubblegum: "linear-gradient(139.73deg, #e4f0f4 0%, #e9d887 100%)",
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
   },
 };
 
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#75332e",
+  secondary: "#be7434",
   background: "#343135",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#191326",
   input: "#483f5a",
-  primaryDark: "#0098A1",
+  // primaryDark: "#0098A1",
   tertiary: "#353547",
   text: "#EAE2FC",
   textDisabled: "#666171",
@@ -50,6 +50,6 @@ export const darkColors: Colors = {
   borderColor: "#524B63",
   card: "#27262c",
   gradients: {
-    bubblegum: "linear-gradient(139.73deg, #e4f0f4 0%, #e9d887 100%)",
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
   },
 };
